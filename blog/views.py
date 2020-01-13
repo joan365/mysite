45a7d66c390a4e0cfb248cc48a4481e39e49cab8
joan365 <joan@soler.cat>
2020-01-13 19:46:40 +0100

    Ver 0.8 - A punt per producció

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Post    #importem els model Post del directori acu¡tual
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-    
+    # El resultat (queryset) que amagatzemat a la variable posts
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    #dins les {} relacionem la variable calculada amb la que cal mostrar a la plantilla
+    return render(request, 'blog/post_list.html', {'paltilla_posts': posts})
