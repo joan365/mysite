86cb8d9bbc30d57dd3b47d226c4b0a1e2a212c5a
joan365 <joan@soler.cat>
2020-01-18 02:22:55 +0100

    Versió 1.0.2 Formulari alta i edició

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     re_path(r'^favicon\.ico$',RedirectView.as_view(url='/static/img/favicon.ico')),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
